8919cfba2fddf3e7f6350dc920d09cd72f303635
Carlos Alberto Conde Moreno <carlosalbertocondemoreno@gmail.com>
2021-10-04 21:57:05 -0500

    Funcion agregarRegistro implementada

--- validaciones.js
+++ validaciones.js
@@ -1,15 +1,21 @@
+let registros = [];
+
 const myForm = document.getElementById("form_nuevo_usuario");
 const nombreUsuario = document.getElementById("dato_nombre_usuario");
-const edad = document.getElementById("dato_nombre_usuario");
-const pasword=document.getElementById("dato_contrasena");
+const edad = document.getElementById("dato_edad_usuario");
+const password = document.getElementById("dato_contrasena");
 
 const submitForm = (e) => {
 	e.preventDefault();
-	if (validar_nombre_usuario(nombreUsuario.value) && validar_edad_usuario(edad.value) && dato_contrasena(pasword.value)) {
-		console.info("Informacion validada con exito");
+	console.log(nombreUsuario.value, edad.value, password.value);
+	if (
+		validar_nombre_usuario(nombreUsuario.value) &&
+		validar_edad_usuario(edad.value) &&
+		validar_contrasena(password.value)
+	) {
+		agregarRegistro();
 		return true;
 	} else {
-		console.info("los datos son incorrectos");
 		return false;
 	}
 };
@@ -29,31 +35,39 @@ function validar_nombre_usuario(string) {
 	if (string.length === 0) return false;
 
 	if (regexp.test(string)) {
-		console.info(`${string} paso la validacion`);
 		return true;
 	}
-	console.error(`${string} no paso la validacion`);
 	return false;
 }
 
 function validar_edad_usuario(edad) {
-	var edad2 = edad.value;
-
-	if (edad2 > 13 && edad2 < 110) {
+	if (edad > 13 && edad < 110) {
 		return true;
 	}
+
+	return false;
 }
-function validar_contrasena(string){
+
+function validar_contrasena(string) {
 	const regexp = /\w{6}/g;
-	if(regexp.test(string)){
-		console.log(`contraseña valida ${string}`);
+	if (regexp.test(string)) {
 		return true;
 	}
 	return false;
+}
+
+function agregarRegistro() {
+	registros.push({
+		usuario: nombreUsuario.value,
+		edad: edad.value,
+		contrasena: password.value,
+	});
+
+	console.table(registros);
 }
 
 myForm.addEventListener("submit", submitForm);
 
 module.exports.validar_nombre_usuario = validar_nombre_usuario;
 module.exports.validar_edad_usuario = validar_edad_usuario;
-module.exports.validar_contrasena=validar_contrasena;
+module.exports.validar_contrasena = validar_contrasena;
