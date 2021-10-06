ce31fb1b716cd25273764ad8de0cad75143d8897
Carlos Alberto Conde Moreno <carlosalbertocondemoreno@gmail.com>
2021-10-05 19:04:44 -0500

    Solucion al sprint 3

--- validaciones.js
+++ validaciones.js
@@ -1,77 +1,85 @@
 let registros = [];
 
-const myForm = document.getElementById("form_nuevo_usuario");
-const nombreUsuario = document.getElementById("dato_nombre_usuario");
-const edad = document.getElementById("dato_edad_usuario");
-const password = document.getElementById("dato_contrasena");
+// const myForm = document.getElementById("form_nuevo_usuario");
+// const nombreUsuario = document.getElementById("dato_nombre_usuario");
+// const edad = document.getElementById("dato_edad_usuario");
+// const password = document.getElementById("dato_contrasena");
 
-const submitForm = (e) => {
-	e.preventDefault();
+// const submitForm = (e) => {
+// 	e.preventDefault();
 
-	if (
-		validar_nombre_usuario(nombreUsuario.value) &&
-		validar_edad_usuario(edad.value) &&
-		validar_contrasena(password.value)
-	) {
-		agregarRegistro();
-		myForm.reset();
-		nombreUsuario.focus();
-		return true;
-	} else {
-		return false;
-	}
-};
+// 	if (
+// 		validar_nombre_usuario(nombreUsuario.value) &&
+// 		validar_edad_usuario(edad.value) &&
+// 		validar_contrasena(password.value)
+// 	) {
+// 		agregarRegistro();
 
-function validar_nombre_usuario(string) {
-	// const regexp = /^[A-Z]{1}([a-zA-Z]){2,12}$/g;
-	// const regexp =
-	// 	/\b([A-Z]{1}[a-z]{1,30}[- ]{0,1}|[A-Z]{1}[- \']{1}[A-Z]{0,1}[a-z]{1,30}[- ]{0,1}|[a-z]{1,2}[ -\']{1}[A-Z]{1}[a-z]{1,30}){2,5}/;
-	const regexp = /^[A-Z]{1}[a-z]*(\s[A-Z]{1}[a-z]*)*\S$/g;
+// 		let arrayOrdenado = OrdenarArreglo(registros);
+// 		console.table(arrayOrdenado);
 
-	for (let i = 0; i < 10; i++) {
-		if (string.includes(i)) {
-			return true;
-		}
-	}
+// 		myForm.reset();
+// 		nombreUsuario.focus();
 
-	if (string.length === 0) return false;
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// };
 
-	if (regexp.test(string)) {
-		return true;
-	}
-	return false;
-}
+// function validar_nombre_usuario(string) {
+// 	// const regexp = /^[A-Z]{1}([a-zA-Z]){2,12}$/g;
+// 	// const regexp =
+// 	// 	/\b([A-Z]{1}[a-z]{1,30}[- ]{0,1}|[A-Z]{1}[- \']{1}[A-Z]{0,1}[a-z]{1,30}[- ]{0,1}|[a-z]{1,2}[ -\']{1}[A-Z]{1}[a-z]{1,30}){2,5}/;
+// 	const regexp = /^[A-Z]{1}[a-z]*(\s[A-Z]{1}[a-z]*)*\S$/g;
 
-function validar_edad_usuario(edad) {
-	if (edad > 13 && edad < 110) {
-		return true;
-	}
+// 	for (let i = 0; i < 10; i++) {
+// 		if (string.includes(i)) {
+// 			return true;
+// 		}
+// 	}
 
-	return false;
-}
+// 	if (string.length === 0) return false;
 
-function validar_contrasena(string) {
-	const regexp = /\w{6}/g;
-	if (regexp.test(string)) {
-		return true;
-	}
-	return false;
-}
+// 	if (regexp.test(string)) {
+// 		return true;
+// 	}
+// 	return false;
+// }
+
+// function validar_edad_usuario(edad) {
+// 	if (edad > 13 && edad < 110) {
+// 		return true;
+// 	}
+
+// 	return false;
+// }
+
+// function validar_contrasena(string) {
+// 	const regexp = /\w{6}/g;
+// 	if (regexp.test(string)) {
+// 		return true;
+// 	}
+// 	return false;
+// }
 
 function agregarRegistro() {
 	registros.push({
-		usuario: nombreUsuario.value,
-		edad: edad.value,
-		contrasena: password.value,
+		usuario: document.getElementById("dato_nombre_usuario").value,
+		edad: document.getElementById("dato_edad_usuario").value,
+		contrasena: document.getElementById("dato_contrasena").value,
 	});
-
-	console.table(registros);
 }
 
-myForm.addEventListener("submit", submitForm);
+function OrdenarArreglo(arreglo) {
+	return arreglo.sort((a, b) => a.edad - b.edad);
+}
+
+// myForm.addEventListener("submit", submitForm);
 
 module.exports.registros = registros;
+module.exports.OrdenarArreglo = OrdenarArreglo;
 module.exports.agregarRegistro = agregarRegistro;
-module.exports.validar_nombre_usuario = validar_nombre_usuario;
-module.exports.validar_edad_usuario = validar_edad_usuario;
-module.exports.validar_contrasena = validar_contrasena;
+// module.exports.validar_nombre_usuario = validar_nombre_usuario;
+// module.exports.validar_edad_usuario = validar_edad_usuario;
+// module.exports.validar_contrasena = validar_contrasena;
